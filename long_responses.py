f75d6726fd68101f3ca89eb38872042cb7c86e10
Khushi Lodha <95740285+khushilodha@users.noreply.github.com>
2024-04-19 21:14:56 +0530

    Update long_responses.py

--- long_responses.py
+++ long_responses.py
@@ -4,7 +4,7 @@ R_EATING = "I don't like eating anything because I'm a bot obviously!"
 R_ADVICE = "If I were you, I would go to the internet and type exactly what you wrote there!"
 
 
-def unknown():
+def unknown()
     response = ["Could you please re-phrase that? ",
                 "...",
                 "Sounds about right.",
