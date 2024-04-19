260ffe1bf06750bb8ebe32f78c03f658430befa9
Khushi Lodha <95740285+khushilodha@users.noreply.github.com>
2024-04-19 21:15:45 +0530

    Update long_responses.py

--- long_responses.py
+++ long_responses.py
@@ -5,7 +5,7 @@ R_ADVICE = "If I were you, I would go to the internet and type exactly what you 
 
 
 def unknown():
-    response = ["Could you please re-phrase that? ",
+    response = ["Could you please re-phrase that? "
                 "...",
                 "Sounds about right.",
                 "What does that mean?"][
